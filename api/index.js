b510ddf0bad393b8acbb1d668f15bf1ddd0f6d1d
hmmm <hmaeyama0510@gmail.com>
2024-07-28 14:03:39 +0900

    feat: connect to the DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
+  console.log("MongoDB is successfully connected.");
+}
+).catch((err) => {
+  console.log(err);
+});
 
 const app = express();
 
