5e965ba2b54063f70688a05462b4673c6c7d0a5b
hmmm <hmaeyama0510@gmail.com>
2024-07-28 14:07:34 +0900

    fix log

--- api/index.js
+++ api/index.js
@@ -14,5 +14,5 @@ mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
 const app = express();
 
 app.listen(3001, () => {
-  console.log("Server is running on port 3001!!!!");
+  console.log("Server is running on port 3001.");
 });
