973d76278e4d6903b2aac8698b8c10e58eaa72c3
hmmm <hmaeyama0510@gmail.com>
2024-07-28 17:54:06 +0900

    add middleware and function for error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,20 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password || name === "" || email === "" || password === "") {
-        return res.status(400).json({ message: "All fields are required." });
+        next(errorHandler(400, "All fields are required."));
     }
 
-    if (name.length > 255) {
-        return res.status(400).json({ message: "Email fields must be less than 255 characters." });
+    if (name.length > 45) {
+        next(errorHandler(400, "Name is too long."));
+    }
+
+    if (email.length > 255) {
+        next(errorHandler(400, "Email is too long."));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -24,6 +29,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json("User signed up successfully.");
     } catch (err) {
-        return res.status(500).json({ message: err.message });
-    }
+        next(err);
+    };
 };
