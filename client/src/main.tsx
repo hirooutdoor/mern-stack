979d0080b2e91fa49196c262081a2c814b4621b0
hmmm <hmaeyama0510@gmail.com>
2024-07-29 17:21:24 +0900

    feat: use redux persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { store } from './app/store.ts';
+import { persistor, store } from './app/store.ts';
 import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <App />
-    </ReduxProvider>
+    <PersistGate persistor={persistor}>
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </PersistGate>
   </React.StrictMode>
 );
