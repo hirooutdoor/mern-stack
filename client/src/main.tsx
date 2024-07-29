bd1a93bcb6e005ceca1c4b0dc34d134c71b4113c
hmmm <hmaeyama0510@gmail.com>
2024-07-29 16:29:36 +0900

    add userSlice for the store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { store } from './app/store.ts';
+import { Provider as ReduxProvider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
+  </React.StrictMode>
+);
