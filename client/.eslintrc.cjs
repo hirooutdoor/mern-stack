a0ab0bb7b4812931c19c3f130f2af64fbaa9e164
hmmm <hmaeyama0510@gmail.com>
2024-07-30 16:54:11 +0900

    chore: utilize node

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,20 +1,20 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "unused-imports"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'unused-imports'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "unused-imports/no-unused-imports": "error",
+    'unused-imports/no-unused-imports': 'error',
   },
 };
