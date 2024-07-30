f02f3626119f42fece042def466bbdf860dfb898
hmmm <hmaeyama0510@gmail.com>
2024-07-30 18:28:04 +0900

    feat: Google OAuth function on server

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,27 +45,73 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-   return next(errorHandler(400, "All fields are required."));
+    return next(errorHandler(400, "All fields are required."));
   }
 
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-     return next(errorHandler(404, "User not found. Please sign up."));
+      return next(errorHandler(404, "User not found. Please sign up."));
     }
 
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-    return  next(errorHandler(400, "Invalid password. Please try again."));
+      return next(errorHandler(400, "Invalid password. Please try again."));
     }
-      
-      const { password:_, ...rest } = validUser._doc;
-    
+
+    const { password: _, ...rest } = validUser._doc;
+
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-      res.status(200).cookie('access_token', token, {httpOnly: true}).json(rest);
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
   } catch (err) {
     next(err);
   }
 
   res.json("User signed in successfully.");
+};
+
+export const google = async (req, res, next) => {
+  const { name, email, googlePhotoUrl } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: _, ...rest } = user._doc;
+
+      res
+        .status(200)
+        .cookie("access_token", token, { httpOnly: true })
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
+      const newUser = new User({
+        name:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hashedPassword,
+        profilePicture: googlePhotoUrl,
+      });
+
+      await newUser.save();
+
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: _, ...rest } = newUser._doc;
+
+      res
+        .status(200)
+        .cookie("access_token", token, { httpOnly: true })
+        .json(rest);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,24 +1,31 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        min: 1,
-        max: 45,
-        unique: true,
+      type: String,
+      required: true,
+      min: 1,
+      max: 45,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        max: 255,
-        unique: true,
+      type: String,
+      required: true,
+      max: 255,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-}, { timestamps: true }
+    profilePicture: {
+      type: String,
+      default:
+        "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=",
+    },
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
