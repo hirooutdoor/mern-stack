755b730927706b5804eb167856b269e5cd2d6e32
hmmm <hmaeyama0510@gmail.com>
2024-08-10 13:48:11 +0900

    add cookie parser for api

--- api/index.js
+++ api/index.js
@@ -3,35 +3,38 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
-  console.log("MongoDB is successfully connected.");
-}
-).catch((err) => {
-  console.log(err);
-});
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => {
+    console.log("MongoDB is successfully connected.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001.");
 });
 
 app.use("/api/user", userRoutes);
-app.use('/api/auth', authRoutes)
-
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  
+
   res.status(statusCode).json({
     success: false,
     statusCode,
-    message
+    message,
   });
 });
